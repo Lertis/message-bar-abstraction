d0ef503ff5be352e97eb3cd1e8946d63291cf75a
Yura Husak <yura.husak@juriba.com>
2021-06-08 19:27:11 +0300

    Issue with HTTP interceptor headers

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { AbstractApiService } from "./abstract.api.service";
@@ -8,19 +8,24 @@ export class ApiService implements AbstractApiService {
 	constructor(private readonly http: HttpClient) { }
 
 	get<T>(url: string): Observable<T> {
-		return this.http.get<T>(url, { headers: { "Access-Control-Allow-Origin": "*" } });
+		return this.http.get<T>(url, { headers: this.getCorsHeaders() });
 	}
 
 	post<T>(url: string, params: T): Observable<T> {
-		return this.http.post<T>(url, params);
+		return this.http.post<T>(url, params, { headers: this.getCorsHeaders() });
 	}
 
 	put<T>(url: string, params: T): Observable<T> {
-		return this.http.put<T>(url, params);
+		return this.http.put<T>(url, params, { headers: this.getCorsHeaders() });
 	}
 
 	delete<T>(url: string): Observable<T> {
-		return this.http.delete<T>(url);
+		return this.http.delete<T>(url, { headers: this.getCorsHeaders() });
+	}
+
+	// Not sure why, but Http interceptor doesn't set CORS headers correctly :(
+	private getCorsHeaders(): HttpHeaders {
+		return new HttpHeaders({ "Access-Control-Allow-Origin": "*" });
 	}
 
 }
